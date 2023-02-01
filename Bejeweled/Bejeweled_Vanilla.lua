09fe93462afded2c975fd748e9c603634ec4513f
Nighthawk <Nighthawk42@users.noreply.github.com>
2023-02-01 03:58:27 -0500

    Wrath/Vanilla Support, Version Bumps
    Added versioning support for Wrath/Vanilla Classic, and bumped TOC versions to current patches.

--- Bejeweled/Bejeweled_Vanilla.lua
+++ Bejeweled/Bejeweled_Vanilla.lua
@@ -1,5 +1,5 @@
 local Bejeweled = Bejeweled or {}
-Bejeweled.version = "Version 10.0.5 - Beta"
+Bejeweled.version = "Version 1.14.3"
 Bejeweled.splashDisplayTime = 2
 local t = "Interface\\AddOns\\Bejeweled"
 local l = "Interface\\AddOns\\Bejeweled\\images\\"
@@ -381,7 +381,7 @@ BejeweledProfile = {
         ["showFlightTooltips"] = 1,
         ["defaultPublish"] = "GUILD"
     },
-    ["version"] = "10.0.5",
+    ["version"] = "1.14.3",
     ["scoresUpdated"] = true,
     ["scoresPopup"] = true,
     ["scoreList"] = {
@@ -3467,7 +3467,7 @@ function Bejeweled:UpdateSavedVariablesDatabase()
     local o = UnitName("player")
     if (BejeweledProfile) then
         if not BejeweledProfile.version then
-            BejeweledProfile.version = "10.0.5"
+            BejeweledProfile.version = "1.14.3"
             BejeweledProfile.scoresUpdated = nil
             BejeweledProfile.scoresPopup = nil
             BejeweledProfile.settings.defaultPublish = "GUILD"
