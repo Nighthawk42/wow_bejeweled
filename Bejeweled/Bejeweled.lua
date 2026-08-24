b6ea9c9befafd3aa613c05923dc356ab1db03ad2
Nighthawk42 <6307495+Nighthawk42@users.noreply.github.com>
2026-08-24 06:02:29 -0400

    Configure client branch for Cataclysm Classic (4.4.2 / 40402)

--- Bejeweled/Bejeweled.lua
+++ Bejeweled/Bejeweled.lua
@@ -1,5 +1,5 @@
 local Bejeweled = Bejeweled or {}
-Bejeweled.version = "Version 12.0.7"
+Bejeweled.version = "Version 4.4.2"
 Bejeweled.splashDisplayTime = 2
 local t = "Interface\\AddOns\\Bejeweled"
 local l = "Interface\\AddOns\\Bejeweled\\images\\"
@@ -381,7 +381,7 @@ BejeweledProfile = {
         ["showFlightTooltips"] = 1,
         ["defaultPublish"] = "GUILD"
     },
-    ["version"] = "12.0.7",
+    ["version"] = "4.4.2",
     ["scoresUpdated"] = true,
     ["scoresPopup"] = true,
     ["scoreList"] = {
@@ -3466,7 +3466,7 @@ function Bejeweled:UpdateSavedVariablesDatabase()
     local o = UnitName("player")
     if (BejeweledProfile) then
         if not BejeweledProfile.version then
-            BejeweledProfile.version = "12.0.7"
+            BejeweledProfile.version = "4.4.2"
             BejeweledProfile.scoresUpdated = nil
             BejeweledProfile.scoresPopup = nil
             BejeweledProfile.settings.defaultPublish = "GUILD"
