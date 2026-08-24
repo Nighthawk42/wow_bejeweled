f08733b571ccaffd7e02ddb179dafbf648541583
Nighthawk42 <6307495+Nighthawk42@users.noreply.github.com>
2026-08-24 06:02:29 -0400

    Configure client branch for Classic Era / Vanilla (1.15.8 / 11509)

--- Bejeweled/Bejeweled.lua
+++ Bejeweled/Bejeweled.lua
@@ -1,5 +1,5 @@
 local Bejeweled = Bejeweled or {}
-Bejeweled.version = "Version 12.0.7"
+Bejeweled.version = "Version 1.15.8"
 Bejeweled.splashDisplayTime = 2
 local t = "Interface\\AddOns\\Bejeweled"
 local l = "Interface\\AddOns\\Bejeweled\\images\\"
@@ -381,7 +381,7 @@ BejeweledProfile = {
         ["showFlightTooltips"] = 1,
         ["defaultPublish"] = "GUILD"
     },
-    ["version"] = "12.0.7",
+    ["version"] = "1.15.8",
     ["scoresUpdated"] = true,
     ["scoresPopup"] = true,
     ["scoreList"] = {
@@ -3466,7 +3466,7 @@ function Bejeweled:UpdateSavedVariablesDatabase()
     local o = UnitName("player")
     if (BejeweledProfile) then
         if not BejeweledProfile.version then
-            BejeweledProfile.version = "12.0.7"
+            BejeweledProfile.version = "1.15.8"
             BejeweledProfile.scoresUpdated = nil
             BejeweledProfile.scoresPopup = nil
             BejeweledProfile.settings.defaultPublish = "GUILD"
